1bd5d0d221fe96b9a909dbd2ce16dc5fcc9f8dc0
Kirill Shekhovtsov <ksedline@gmail.com>
2020-04-22 22:18:39 +0300

    Initial commit

---
+++ precache-manifest.2308ce577e102f316b268bf295a6e818.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79860bca47cd0b49a781970ca1887d75",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1f907834a20f60b0a36e",
+    "url": "/static/css/main.6a1a4fbe.chunk.css"
+  },
+  {
+    "revision": "cea10566eef5f21c9880",
+    "url": "/static/js/2.a6ba076b.chunk.js"
+  },
+  {
+    "revision": "c9e5d1334b64163727874269ed6e8c30",
+    "url": "/static/js/2.a6ba076b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f907834a20f60b0a36e",
+    "url": "/static/js/main.ac049276.chunk.js"
+  },
+  {
+    "revision": "7b2bc2cc9101d0a94cf2",
+    "url": "/static/js/runtime-main.f37ef3a9.js"
+  }
+]);
